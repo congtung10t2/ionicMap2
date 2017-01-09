18e9d0e58d154013782eecb37b366f9286aa2fbc
congtung10t2 <congtung10t2@gmail.com>
2017-01-09 22:16:12 +0700

    add navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
@@ -9,14 +9,27 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = HomePage;
+    @ViewChild(Nav) nav: Nav;
+
+    rootPage: any = HomePage;
+
+    pages: Array<{ title: string, component: any }>;
 
   constructor(platform: Platform) {
+    this.pages = [
+        { title: 'function 1', component: HomePage },
+        { title: 'function 2', component: HomePage }
+     ];
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+  }
+  openPage(page) {
+      // Reset the content nav to have just this page
+      // we wouldn't want the back button to show in this scenario
+      this.nav.setRoot(page.component);
   }
 }
