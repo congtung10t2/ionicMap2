4af646b2305a1da98dff441ba551625921aad1f9
congtung10t2 <congtung10t2@gmail.com>
2017-01-09 23:25:02 +0700

    add_native_map

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+declare var google;
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'home-page',
+    templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
   constructor(public navCtrl: NavController) {
     
+  }
+  ionViewDidLoad() {
+      this.loadMap();
+  }
+  loadMap() {
+
+      let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+      let mapOptions = {
+          center: latLng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
   }
 
 }
